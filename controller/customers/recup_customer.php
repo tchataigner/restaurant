2b3bce5ed6ebfe53890aee42a030a090ce254090
tchataigner <tom.chataigner@yahoo.fr>
2016-10-05 15:34:28 +1000

    add details page

--- controller/customers/recup_customer.php
+++ controller/customers/recup_customer.php
@@ -7,7 +7,11 @@ require "../../class/class_customer.php";
 $items = new Customer();
 $items->setNumber($_POST["number"]);
 $results = $items->retrieve_customer();
-var_dump($results);
-exit();
+
+if (empty($results)) {
+	 	header('Location: ../../views/customer/add_details.php');      
+	} else {
+		/* Here save the number of the customer in the new order table */ 
+	}
 
 ?>
