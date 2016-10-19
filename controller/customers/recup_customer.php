b48f3182ef2e74b7130488c86d3c89f1648d0a84
tchataigner <markelisha2@gmail.com>
2016-10-19 17:50:14 +1000

    mark

--- controller/customers/recup_customer.php
+++ controller/customers/recup_customer.php
@@ -11,10 +11,11 @@ $results = $items->retrieve_customer();
 	exit; */
 
 if (empty($results)) {
+
 	 	header('Location: ../../index.php?page=add_details');
 }
  else {
-
+ 	
  	header('Location: ../../index.php?page=customer_number');
 	/* Here save the number of the customer in the new order table */ 
 	}
--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if ($page == 'home') {
 	include "/views/customer/recup_customer.php";
 } elseif($page == 'add_details'){
 	include "/views/customer/add_details.php";
+}elseif($page == 'choose_items'){
+	include "/views/new_order/choose_items.php"
 }
 
 ?>
