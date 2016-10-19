82cbec11e52c0cf57452e9ad24c83fd2fc90e765
tchataigner <tom.chataigner@yahoo.fr>
2016-10-19 17:03:55 +1000

    income day

---
+++ class/class_income.php
@@ -0,0 +1,20 @@
+<?php
+
+class Income
+{
+
+    public function get_income_day(){
+        $dsn = 'mysql:dbname=restaurant;host=localhost';
+        $user = 'root';
+        $password = '';
+        $pdo = new PDO($dsn, $user, $password);
+        $sql = 'SELECT Price,item_number FROM ITEM,ORDERS WHERE id_item=Id ;';
+        $sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute();
+        $result = $sth->fetchAll();
+
+        return $result;
+    }
+}
+
+?>
